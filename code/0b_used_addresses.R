08acb0494d806da37efc90ac080e320d7eda9ade
Emma Schweidler <emma.schweidler@gov.scot>
2024-02-05 10:35:53 +0000

    add 0b_used_addresses.R file

---
+++ code/0b_used_addresses.R
@@ -0,0 +1,67 @@
+#########################################################################
+# Name of file - 0b_used_addresses.R
+#
+# Type - Reproducible Analytical Pipeline (RAP)
+# Written/run on - RStudio Desktop
+# Version of R - 4.2.2
+#
+# Description - Imports the cleaned postcode address file and 
+# removes previously sampled addresses.
+
+#########################################################################
+
+### 0 - Setup ----
+
+# Run setup script which loads all required packages and functions and 
+# executes the config.R script.
+
+source(here::here("code", "00_setup.R"))
+
+### 1 - Import files ----
+
+# Import all previously sampled address files and 
+# add column with file name
+usedaddresses <-
+  do.call(rbind, lapply(seq_along(previoussamples.path),
+                        function(x)
+                          transform(
+                            haven::read_sas(previoussamples.path[x], 
+                                            col_select = "UDPRN"),
+                            survey = previoussamples.path[x]
+                          )))
+
+### 2 - Process data ----
+
+# Mutate file name to name of survey and clean names
+usedaddresses <- usedaddresses %>%
+  mutate(survey = case_when(str_detect(survey, 
+                                       regex("scjs",
+                                             ignore_case = TRUE)) ~ "scjs",
+                            str_detect(survey, 
+                                       regex("shes", 
+                                             ignore_case = TRUE)) ~ "shes",
+                            str_detect(survey, 
+                                       regex("shs", 
+                                             ignore_case = TRUE)) ~ "shs",
+                            str_detect(survey, 
+                                       regex("ssas", 
+                                             ignore_case = TRUE)) ~ "ssas")) %>%
+  rename(udprn = UDPRN)
+
+# Keep unique udprn and survey combination and select required variables
+usedaddresses <- usedaddresses %>% 
+  group_by(udprn, survey) %>% 
+  count() %>% 
+  select(udprn, survey)
+
+### 3 - Export used addresses  ----
+
+# Code to export used addresses into lookups folder
+write_rds(
+  usedaddresses,
+  here("lookups", paste0(Sys.Date(), "_usedaddresses.rds")),
+  compress = "gz"
+)
+
+
+
