d051beabc9b9ee19b92a5b1f337956b6d97f7f93
Emma Schweidler <emma.schweidler@gov.scot>
2024-02-06 08:30:53 +0000

    add import_multiple_files_csv.R

---
+++ functions/import_multiple_files_csv.R
@@ -0,0 +1,17 @@
+#' Imports multiple csv files and retains filename in new column
+#' 
+#' @param filenames Filenames of all addresses sampled in the last four years.
+#' 
+#' @returns Imports the udprn of previously sampled addresses 
+#' and retains their filename in a new column to allow further inspection.
+#' 
+#' @examples
+#' import_multiple_files_csv(files_del)
+
+import_multiple_files_csv <- function(filenames){
+  df <- read.csv(filenames) %>%
+    clean_names() %>%
+    select(c("udprn"))
+  df$filename <- filenames
+  return(df)
+}
