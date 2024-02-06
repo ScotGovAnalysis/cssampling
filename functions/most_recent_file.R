f0cedfe1bb664ccf4d3f93454c703a4952ad305b
Emma Schweidler <emma.schweidler@gov.scot>
2024-02-06 07:22:13 +0000

    update description

--- functions/most_recent_file.R
+++ functions/most_recent_file.R
@@ -1,13 +1,14 @@
-#' Identify the most recent file
+#' Identify most recent file based on date in filename
 #' 
-#' @param path Path to folder in which the files which are to be 
-#' searched are located.
+#' @param path Path to folder in which files are to be 
+#' searched.
 #' 
 #' @param pattern Only file names matching the pattern will be returned. 
 #' To return all files, use "".
 #' 
 #' @returns The file name of the most recent file in the path folder 
-#' which matches the specified pattern.
+#' which matches the specified pattern. Filenames must include a date 
+#' for the function to work properly.
 #' 
 #' @examples
 #' most_recent_file(getwd(), "paf")
