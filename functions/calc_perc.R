74bf2b79f6602fd39862e95288f9ba1e9fef6771
Emma Schweidler <emma.schweidler@gov.scot>
2024-02-14 11:47:25 +0000

    add calc_perc.R function

---
+++ functions/calc_perc.R
@@ -0,0 +1,20 @@
+#' Calculate percentages of grouping variable
+#' 
+#' @param df Data frame which includes the grouping variable.
+#' 
+#' @param grouping_variable Variable which is used to group the data frame.
+#' 
+#' @returns Data frame with counts and percentages of the 
+#' grouping_variable.
+#' 
+#' @examples
+#' calc_perc(contractor.sample, dz11_urbrur2020)
+
+
+calc_perc <- function(df, grouping_variable) {
+  group <- enquo(grouping_variable)
+  df %>% 
+  group_by(!!!group) %>% 
+  summarise(n = n()) %>%
+  mutate(perc = n/sum(n)*100)
+}
