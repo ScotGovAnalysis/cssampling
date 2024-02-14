aa64887fa7f3eafd24f7caf4e1749a349282a42e
Emma Schweidler <emma.schweidler@gov.scot>
2024-02-14 11:50:10 +0000

    add prev_cur_comp.R function

---
+++ functions/prev_cur_comp.R
@@ -0,0 +1,28 @@
+#' Compares distributions of current and previous samples.
+#' 
+#' @param current_df Current contractor sample.
+#' 
+#' @param previous_df Last year's contractor sample.
+#' 
+#' @returns A data frame which includes the distribution of a variable in
+#' the current contractor sample and the previous year's contractor sample.
+#' The diff column shows the difference between both samples.
+#' 
+#' @examples
+#' prev_cur_comp(contractor.la.urbrur, contractor.previous.la.urbrur)
+
+prev_cur_comp <- function(current_df, previous_df) {
+  
+  # calculate different between current and previous samples
+  diff <- cbind(diff = current_df[,-1] - previous_df[,-1]) %>%
+  rename_with(.fn = \(x)sub("diff.sample", "diff", x))
+  
+  # add prefix to column names to identify samples
+  colnames(current_df)[-1] <- paste0("contractor_", colnames(current_df)[-1])
+  colnames(previous_df)[-1] <- paste0("previous_", colnames(previous_df)[-1])
+  
+  # combine data frames into one
+  qa <- cbind(current_df, previous_df[,-1], diff)
+  
+  return(qa)
+}
